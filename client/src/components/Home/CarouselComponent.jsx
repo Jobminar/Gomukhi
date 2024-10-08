2bcad3edda795045ac28cf1ee9522c505edbbcfb
sameerg1810 <64128604+sameerg1810@users.noreply.github.com>
2024-10-08 10:26:40 +0530

    added the Home CarousalComponent and logo.svg

--- client/src/components/Home/CarouselComponent.jsx
+++ client/src/components/Home/CarouselComponent.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Carousel from "react-bootstrap/Carousel";
 import "bootstrap/dist/css/bootstrap.min.css";
 import carouselImage from "./assets/images/carousalimg1.jpg"; // Ensure this path is correct
+import logoSvg from "./assets/images/logo.svg"; // Import the logo
 import "./styles/Carousal.css";
 
 const CarouselComponent = () => {
@@ -9,14 +10,23 @@ const CarouselComponent = () => {
     <Carousel className="carousel-container">
       <Carousel.Item>
         <img className="d-block w-100" src={carouselImage} alt="First slide" />
+        <div className="carousel-logo-container">
+          <img src={logoSvg} alt="Logo" className="carousel-logo" />
+        </div>
         <Carousel.Caption></Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
         <img className="d-block w-100" src={carouselImage} alt="Second slide" />
+        <div className="carousel-logo-container">
+          <img src={logoSvg} alt="Logo" className="carousel-logo" />
+        </div>
         <Carousel.Caption></Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
         <img className="d-block w-100" src={carouselImage} alt="Third slide" />
+        <div className="carousel-logo-container">
+          <img src={logoSvg} alt="Logo" className="carousel-logo" />
+        </div>
         <Carousel.Caption></Carousel.Caption>
       </Carousel.Item>
     </Carousel>
